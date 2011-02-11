fc5db9605da907e9e27d387814a8a6ad9f168d68
VersionBoy <versionboy@yahoo.com>
2011-02-11 22:48:20 +0000

    git-svn-id: svn://svn.code.sf.net/p/ekeyfinder/code/trunk@75 b14f650f-2e60-4330-827c-04cc947f8aee

--- VersionConsts.pas
+++ VersionConsts.pas
@@ -4,10 +4,6 @@
 
     Copyright (C) 2011 Enchanted Keyfinder Project
 
-    The Initial Developer of this Original Code is VersionBoy.
-    Portions created by VersionBoy are Copyright (C) 2011 VersionBoy.
-    All Rights Reserved. 
- 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -21,7 +17,11 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Contributor(s): 
+    The initial developer of this code is VersionBoy.
+    Portions created by VersionBoy are Copyright (C) 2011.
+    All Rights Reserved.
+
+    Contributor(s): VersionBoy.
 }
 unit VersionConsts;
 
