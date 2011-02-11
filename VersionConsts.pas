85aa836562fa74738d09b356f94233ae61ff3cf8
VersionBoy <versionboy@yahoo.com>
2011-02-11 08:43:48 +0000

    Progran information
    git-svn-id: svn://svn.code.sf.net/p/ekeyfinder/code/trunk@60 b14f650f-2e60-4330-827c-04cc947f8aee

---
+++ VersionConsts.pas
@@ -0,0 +1,42 @@
+{
+    The Enchanted Keyfinder is a free utility that retrieves your
+    Product Key (cd key) used to install Windows from your registry.
+
+    Copyright (C) 2011 Enchanted Keyfinder Project
+
+    The Initial Developer of this Original Code is VersionBoy.
+    Portions created by VersionBoy are Copyright (C) 2011 VersionBoy.
+    All Rights Reserved. 
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contributor(s): 
+}
+unit VersionConsts;
+
+interface
+
+const
+  PROGRAM_NAME = 'Enchanted Keyfinder';
+  PROGRAM_VERSION = '0.1 Beta 4';
+  PROGRAM_STABLE_VERSION = '0.0';
+  PROGRAM_RELEASE_DATE = 'January 20th, 2011';
+  PROGRAM_HOME_PAGE = 'http://';
+  PROGRAM_SOURCEFORGE_HOME_PAGE = 'http://sourceforge.net/projects/keyfinder/';
+  PROGRAM_SOURCEFORGE_PHPBB_PAGE = 'http://sourceforge.net/apps/phpbb/keyfinder/';
+  PROGRAM_SVN_TRUNK = 'http://keyfinder.svn.sourceforge.net/svnroot/keyfinder/trunk/keyfinder/';
+
+implementation
+
+end.
